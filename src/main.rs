51e74a1d9f0172de2cd60240c9eb821ffe6b7c57
Yassine ben ahmed <yassinebenahmede@gmail.com>
2023-07-18 23:44:27 +0100

    part 2 done

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ fn main() {
     let mut sum_score: u64 = 0;
     for line in data.lines(){
         let letters_vec: Vec<&str> = line.split_whitespace().collect();
-        match letters_vec[1]{
+        let condition = match letters_vec[1] {
+            "X"=>WinEnum::Loss,
+            "Y"=>WinEnum::Draw,
+            "Z"=>WinEnum::Win,
+            _=>panic!("EEEEEEEEEEEEE")
+        };
+        let choice = choice_picker(&letters_vec, &condition);
+        match choice{
             "X"=> sum_score+=1,
             "Y"=> sum_score+=2,
             "Z"=> sum_score+=3,
             _=> println!("Weird bug")
         }
-        match is_win(&letters_vec) {
+        match condition{
             WinEnum::Draw=> sum_score+=3,
             WinEnum::Win=> sum_score+=6,
             _=>sum_score+=0
@@ -48,6 +55,30 @@ fn is_win(played_vec: &Vec<&str>)->WinEnum{
             _=> return WinEnum::Loss 
         }
     } else {return WinEnum::Loss}
+}
+
+fn choice_picker<'a>(played_vec: &'a Vec<&'a str>, condition: &WinEnum)->&'a str{
+    if played_vec[0] == "A"{
+        match condition{
+            WinEnum::Win=> return "Y",
+            WinEnum::Draw=> return "X",
+            WinEnum::Loss=> return "Z"
+        }
+    } else if played_vec[0] == "B"{
+        match condition{
+            WinEnum::Win=> return "Z",
+            WinEnum::Draw=> return "Y",
+            WinEnum::Loss=> return "X"
+        }
+    } else if played_vec[0] == "C"{
+        match condition {
+            WinEnum::Win=> return "X",
+            WinEnum::Draw=> return "Z",
+            WinEnum::Loss=>"Y"
+        }
+    } else {
+        panic!("WTF!!")
+    }
 }
 
 
